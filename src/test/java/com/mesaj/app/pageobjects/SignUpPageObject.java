82959aab51f4a3ca54be210d86b01503d2280f7e
David Cubillo <davidcubillo@gmail.com>
2020-10-25 17:25:52 -0600

    commit de cambios

---
+++ src/test/java/com/mesaj/app/pageobjects/SignUpPageObject.java
@@ -0,0 +1,49 @@
+package com.mesaj.app.pageobjects;
+
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class SignUpPageObject {
+
+    @Autowired
+    public SignUpPageObject(WebDriver driver)
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath =" //input[@ng-model='FirstName']" )
+    private WebElement firstNameTextBox;
+
+    @FindBy(xpath ="//input[@ng-model='LastName']")
+    private WebElement lastNameTextBox;
+    @FindBy(tagName ="textarea")
+    private WebElement addressTextBox;
+    @FindBy(xpath ="//input[@type='email']")
+    private WebElement emailTextBox;
+    @FindBy(xpath ="//input[@type='tel']")
+    private WebElement phoneTextBox;
+    @FindBy(xpath ="//input[@value='Male']")
+    private WebElement GenderMaleRadioButton;
+    @FindBy(id ="countries")
+    private WebElement countries;
+    @FindBy(id ="yearbox")
+    private WebElement year;
+    @FindBy(xpath ="//select[@placeholder='Month']")
+    private WebElement month;
+    @FindBy(id ="daybox")
+    private WebElement day;
+    @FindBy(id ="firstpassword")
+    private WebElement password;
+    @FindBy(id ="secondpassword")
+    private WebElement confirmPassword;
+    @FindBy(id ="submitbtn")
+    private WebElement submitbutton;
+
+}
---
+++ src/test/java/com/mesaj/app/stepdefinitions/SignUpStepDefs.java
@@ -0,0 +1,50 @@
+package com.mesaj.app.stepdefinitions;
+
+import com.mesaj.app.conf.DriverConfig;
+import com.mesaj.app.pageobjects.SignUpServices;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+//todo remove from the step definitions
+@ContextConfiguration(classes = {DriverConfig.class})
+public class SignUpStepDefs {
+
+    @Autowired
+    private SignUpServices signUp;
+
+    @Given("Pepito wants to have an account")
+    public void pepito_wants_to_have_an_account() throws InterruptedException {
+        signUp.go("http://demo.automationtesting.in/Register.html");
+        signUp.writeFirstName("David");
+        signUp.writeLastName("Smith");
+        signUp.writeAddress("Zarcero");
+        signUp.writeEmail("davidcubillo@gmail.com");
+        signUp.writePhone();
+        signUp.clickGender();
+        signUp.selectCountry("Bolivia");
+        signUp.selectYear("1984");
+        signUp.selectMonth("October");
+        signUp.selectDay("14");
+        signUp.writePassword("Cualquiera1984");
+        signUp.writeConfirmPassword("Cualquiera1984");
+        signUp.clickSubmitButton();
+        Thread.sleep(4000);
+    }
+    @When("^he sends required information to get the account$")
+    public void he_sends_required_information_to_get_the_account() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he should be told that the account was created$")
+    public void he_should_be_told_that_the_account_was_created() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
