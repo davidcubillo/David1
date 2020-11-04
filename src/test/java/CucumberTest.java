85bb9808f2e17598941f88c978915b298458ca9e
David Cubillo <david.cubillo@rivetlogic.com>
2020-11-04 17:51:49 -0600

    More changes

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -6,7 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
-        plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" }
+        plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber",
+                   "com.mesaj.app.util.driver.reporting.JListener"}
 
   )
 public class CucumberTest {
--- src/test/java/com/mesaj/app/util/driver/reporting/JListener.java
+++ src/test/java/com/mesaj/app/util/driver/reporting/JListener.java
@@ -1,2 +1,36 @@
-package com.mesaj.app.util.driver.reporting;public class JListener {
+package com.mesaj.app.util.driver.reporting;
+
+import io.cucumber.plugin.ConcurrentEventListener;
+import io.cucumber.plugin.event.EventPublisher;
+import io.cucumber.plugin.event.Result;
+import io.cucumber.plugin.event.Status;
+import io.cucumber.plugin.event.TestCase;
+import io.cucumber.plugin.event.TestCaseFinished;
+import io.cucumber.plugin.event.TestCaseStarted;
+import io.cucumber.plugin.event.TestStep;
+import io.cucumber.plugin.event.TestStepFinished;
+
+public class JListener implements ConcurrentEventListener {
+
+  public void onTestCaseStarted(TestCaseStarted event)
+  {
+    TestCase testCase = event.getTestCase();
+    System.out.println("Starting "+ testCase.getName());
+  }
+  private void onTestStepFinished(final TestCaseFinished event)
+  {
+    TestCase finishTest = event.getTestCase();
+    System.out.println("Finished "+ finishTest.getName());
+    Result result = event.getResult();
+    if (result.getStatus() == Status.FAILED)
+    {
+      final Throwable error = result.getError();
+      error.printStackTrace();
+    }
+  }
+  @Override
+  public void setEventPublisher(EventPublisher publisher) {
+    publisher.registerHandlerFor(TestCaseStarted.class, this::onTestCaseStarted);
+    publisher.registerHandlerFor(TestCaseFinished.class, this::onTestStepFinished);
+  }
 }
